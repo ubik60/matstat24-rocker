bcfed19e338f33994230e223a099830d9630cebe
Anders Johansson <ajj@hig.se>
2024-09-20 12:23:17 +0200

    Update install.R
    tidyverse flextable remotes speegled/fosdata packages

--- install.R
+++ install.R
@@ -1,1 +1,7 @@
-install.packages("ggplot2")
+    install.packages("tidyverse")
+    install.packages("flextable")
+    install.packages("remotes")
+    remotes::install_github(repo = "speegled/fosdata")
+
+    install.packages("tinytex")
+    tinytex::install_tinytex()
