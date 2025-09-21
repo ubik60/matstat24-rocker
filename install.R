7c0b033a1a26f2d5a9a274df453e233b15a4793a
Anders Johansson <ajj@hig.se>
2025-09-21 21:39:29 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,5 @@
     install.packages("tidyverse")
+    install.packages("ggdensity")
     install.packages("flextable")
     install.packages("remotes")
     remotes::install_github(repo = "speegled/fosdata")
